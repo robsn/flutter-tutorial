c8bb06c7f5b9b44a2c838211e071a95a3bac56d7
Robin Vinzenz <info@robinvinzenz.de>
2019-11-07 17:00:31 +0100

    Transform App into todo app

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-      title: 'Name Generator',
-      theme: ThemeData(
-        primaryColor: Colors.blueAccent
-      ),
-      home: RandomWords(),
-  );
+        title: 'Name Generator',
+        theme: ThemeData(primaryColor: Colors.blueAccent),
+        home: TodoItem(),
+      );
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final List<WordPair> _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();
+class TodoItemState extends State<TodoItem> {
+  final List<String> _todoItems = <String>[];
+
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
 
-  Widget _buildSuggestions() {
-    return ListView.builder(
+  Widget _buildTodoList() {
+    return ListView.separated(
+        separatorBuilder: (context, index) => Divider(
+          color: Colors.black45,
+        ),
+        itemCount: _todoItems.length,
         padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider();
-          /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        itemBuilder: (context, i) {
+          if (i < _todoItems.length) {
+            return _buildRow(_todoItems[i]);
           }
-          return _buildRow(_suggestions[index]);
         });
   }
 
@@ -38,69 +34,66 @@ class RandomWordsState extends State<RandomWords> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-        ],
+        title: Text('Your Todos'),
       ),
-      body: _buildSuggestions(),
+      body: _buildTodoList(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _goToAddNew();
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blueAccent,
+      ),
     );
   }
 
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
+  Widget _buildRow(String pair) {
     return ListTile(
       title: Text(
-        pair.asPascalCase,
+        pair,
         style: _biggerFont,
       ),
-      trailing: Icon(
-        // Add the lines from here...
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
     );
   }
 
-  void _pushSaved() {
-    Navigator.of(context).push(MaterialPageRoute<void>(
-      builder: (BuildContext context) {
-        final Iterable<ListTile> tiles = _saved.map(
-          (WordPair pair) {
-            return ListTile(
-              title: Text(
-                pair.asPascalCase,
-                style: _biggerFont,
-              ),
-            );
-          },
-        );
+  void _goToAddNew() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('Saved Suggestions'),
+        ),
+        body: Container(
+            padding: const EdgeInsets.all(8.0),
+            alignment: Alignment.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text("Add new todo"),
+                TextField(
+                  autofocus: true,
+                  onSubmitted: (val) {
+                    _addTodoItem(val);
+                    Navigator.pop(context);
+                  },
+                  decoration: new InputDecoration(
+                      hintText: 'Enter something to do...',
+                      contentPadding: const EdgeInsets.all(16.0)),
+                )
+              ],
+            )),
+      );
+    }));
+  }
 
-        final List<Widget> divided =
-            ListTile.divideTiles(context: context, tiles: tiles).toList();
-
-        return Scaffold(         // Add 6 lines from here...
-          appBar: AppBar(
-            title: Text('Saved Suggestions'),
-          ),
-          body: ListView(children: divided),
-        );
-
-      },
-    ));
+  void _addTodoItem(String todoName) {
+    setState(() {
+      _todoItems.add(todoName);
+    });
   }
 }
 
-class RandomWords extends StatefulWidget {
+class TodoItem extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => RandomWordsState();
+  State<StatefulWidget> createState() => TodoItemState();
 }
